308a3b4c53cc654a2905551101b2c34bb5de21ec
Tarrantino <victorsunlixin@gmail.com>
2015-11-08 15:25:33 +0800

    layout

--- app/src/main/java/zhang/zhixuan/mobileapp_airline/FacebookAccountPage.java
+++ app/src/main/java/zhang/zhixuan/mobileapp_airline/FacebookAccountPage.java
@@ -6,8 +6,14 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
+import com.facebook.AccessToken;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
 import com.facebook.Profile;
 import com.facebook.login.widget.ProfilePictureView;
 
@@ -15,11 +21,12 @@ public class FacebookAccountPage extends Activity {
 
     TextView facebookAccount_tv_profile;
     Fragment facebookFragment;
+    Profile profile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facebook_account_page);
-
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction;
 
@@ -29,7 +36,7 @@ public class FacebookAccountPage extends Activity {
         fragmentTransaction.add(R.id.facebookAccount_fl_facebook, facebookFragment);
         fragmentTransaction.commit();
 
-        Profile profile = Profile.getCurrentProfile();
+        profile = Profile.getCurrentProfile();
         facebookAccount_tv_profile = (TextView)findViewById(R.id.facebookAccount_tv_profile);
         facebookAccount_tv_profile.setText(profile.getName());
         ProfilePictureView profilePictureView;
@@ -46,6 +53,28 @@ public class FacebookAccountPage extends Activity {
         facebookFragment.onActivityResult(requestCode, resultCode, data);
     }
 
+    public void facebookLogin(View view) {
+//            new GraphRequest(
+//                    AccessToken.getCurrentAccessToken(),
+//                    "/{user-id}",
+//                    null,
+//                    HttpMethod.GET,
+//                    new GraphRequest.Callback() {
+//                        public void onCompleted(GraphResponse response) {
+//                            if(response != null){
+//                                try {
+//
+//                                } catch (Exception e){
+//
+//                                }
+//                            }
+//                        }
+//                    }
+//            ).executeAsync();
+        Intent i = new Intent(this,AccountManagementPage.class);
+        i.putExtra("email","zhangzhixuan312@gmail.com");
+        startActivity(i);
+        }
 
 
 }
